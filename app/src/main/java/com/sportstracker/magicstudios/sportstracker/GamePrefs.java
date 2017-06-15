cb9c4d70a867d146b32402b2734e0a3babced4d5
James Inbody <jinbody1@gmail.com>
2017-06-14 21:34:45 -0400

    add some constants for the prefs to be saved

--- app/src/main/java/com/sportstracker/magicstudios/sportstracker/GamePrefs.java
+++ app/src/main/java/com/sportstracker/magicstudios/sportstracker/GamePrefs.java
@@ -6,4 +6,11 @@ package com.sportstracker.magicstudios.sportstracker;
  */
 
 public class GamePrefs {
+
+    public static final String HOME_SCORE ="homeScore";
+    public static final String VISITOR_SCORE ="visitorScore";
+    public static final String TOP_BOTTOM_INNING = "top_bottom";
+    public static final String INNING_TOP = "top";
+    public static final String INNING_BOTTOM = "bottom";
+
 }
