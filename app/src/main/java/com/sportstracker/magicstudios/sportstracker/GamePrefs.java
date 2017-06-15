6c6f9a6024ceb54ff43d5c2196788cd451428941
James Inbody <jinbody1@gmail.com>
2017-06-14 21:09:06 -0400

    create java class for game preferences

---
+++ app/src/main/java/com/sportstracker/magicstudios/sportstracker/GamePrefs.java
@@ -0,0 +1,9 @@
+package com.sportstracker.magicstudios.sportstracker;
+
+/**
+ * Created by jinbody on 6/14/2017.
+ * Wrapper for creating, adding, and editing SharedPreferences for a certain game
+ */
+
+public class GamePrefs {
+}
